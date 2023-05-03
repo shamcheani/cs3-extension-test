bb9d2d746fe308bd09c04a4c0a01367a5dcf3129
duongnv1996 <duongnv1996@gmail.com>
2023-05-04 00:11:54 +0700

    commit main phimmoi

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,8 +1,8 @@
 package com.example
 
-import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.ui.home.ParentItemAdapter
+import org.jsoup.Jsoup
 
 class ExampleProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://phimmoichillc.net/"
@@ -17,5 +17,34 @@ class ExampleProvider : MainAPI() { // all providers must be an instance of Main
     // this function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf<SearchResponse>()
+    }
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse? {
+        val html = app.get(mainUrl).text
+        val doc = Jsoup.parse(html)
+        val listHomePageList = arrayListOf<HomePageList>()
+        doc.select(".block").forEach {
+            val name = it.select(".caption").text().trim()
+            val urlMore = fixUrl(it.select(".see-more").attr("href"))
+            val listMovie = it.select(".list-film .item").map {
+                val title = it.select("p").last()!!.text()
+                val href = fixUrl(it.selectFirst("a")!!.attr("href"))
+                val year = 0
+                val image = it.selectFirst("img")!!.attr("src")
+                MovieSearchResponse(
+                    title,
+                    href,
+                    this.name,
+                    TvType.Movie,
+                    image,
+                    year,
+                    posterHeaders = mapOf("referer" to mainUrl)
+                )
+            }
+            if (listMovie.isNotEmpty())
+                listHomePageList.add(HomePageList(name, listMovie ))
+        }
+
+        return HomePageResponse(listHomePageList)
     }
 }
